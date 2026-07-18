6aae04adbf51c9d960a7f677f7bee32e7569fa66
soumyajitITis <soumyajitdhar50@gmail.com>
2026-07-18 23:11:07 +0530

    bug fix

--- src/components/shared/SharedPhoneInput.tsx
+++ src/components/shared/SharedPhoneInput.tsx
@@ -1,5 +1,5 @@
 import { Controller, useFormContext } from 'react-hook-form';
-import PhoneInput, { CountryCode } from 'react-phone-number-input';
+import PhoneInput from 'react-phone-number-input';
 import 'react-phone-number-input/style.css';
 
 interface Props {
@@ -17,7 +17,7 @@ export default function SharedPhoneInput({ name, value, onChange, disabled, defa
   const phoneInputClasses = `flex w-full px-3 py-2 bg-white dark:bg-[#0f1115] border border-gray-300 dark:border-white/10 rounded-sm text-sm text-gray-900 dark:text-white focus-within:ring-2 focus-within:ring-[#792359]/50 transition-colors [&>input]:bg-transparent [&>input]:focus:outline-none [&>input]:w-full ${className}`;
 
   const safeDefaultCountry = (typeof defaultCountry === 'string' && /^[A-Z]{2}$/.test(defaultCountry))
-    ? (defaultCountry as CountryCode)
+    ? (defaultCountry as any)
     : undefined;
 
   if (formContext && name) {
